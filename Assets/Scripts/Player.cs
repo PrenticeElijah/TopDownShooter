71017271dd4a1eafe51d03f516b968c52c92904e
elijahprentice <prentee1213@gmail.com>
2020-12-22 16:10:11 -0500

    Player and Tilemap

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public float moveSpeed;
+    Rigidbody2D playerRig;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") != 0)
+        {
+            playerRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * (moveSpeed / 1.5f), Input.GetAxisRaw("Vertical") * (moveSpeed / 1.5f));
+        }
+        else if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            playerRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+        else
+        {
+            playerRig.velocity = new Vector2(0,0);
+        }
+    }
+
+    void Shoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            Debug.Log("Shooting");
+    }
+}
